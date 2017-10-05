5825f5fb4e87d44eae35215406a8a5b164ae57db
jeremyanywhere <jeremycustomer@gmail.com>
2017-10-05 17:03:09 -0400

    Updates..

--- pykersite/poker/apps.py
+++ pykersite/poker/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class PokerConfig(AppConfig):
     name = 'poker'
+    utilities_url = 'localhost etc'
+    NANCY = 'Nancy defined here..'
+    print ("Well here we are in the PokerConfig class.. maybe we should define stuff here?")
